bb3d0559a8498994af67e2e45639cab6e3cfa082
rizaleff <rizaleffendi92@gmail.com>
2023-09-25 15:19:52 +0700

    Data Annotation

--- API/Models/University.cs
+++ API/Models/University.cs
@@ -1,8 +1,14 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
 {
+    [Table("tb_m_universities")]
     public class University : GeneralModel
     {
+        //Tipe data numeric atau selain string tidak perlu menentukan type name nya
+        [Column("code", TypeName = "nvarchar(50)")]
         public string Code { get; set; }
+        [Column("name", TypeName = "nvarchar(100)")] //Bisa juga menggunakan MaxLength 
         public string Name { get; set; }
 
     }
